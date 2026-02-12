805f7ec8b2b93f9857853b644cfe2361307e6b84
enumerate <3554346672@qq.com>
2026-02-12 20:44:27 +0800

    1.6.6

--- src/main/java/com/enumerate/disease_detection/POJO/DTO/ExecutionPlanDTO.java
+++ src/main/java/com/enumerate/disease_detection/POJO/DTO/ExecutionPlanDTO.java
@@ -1,5 +1,6 @@
 package com.enumerate.disease_detection.POJO.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExecutionPlanDTO {
 
     /**
